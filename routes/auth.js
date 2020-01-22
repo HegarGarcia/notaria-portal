44d720d99e21a89ea5d7c982fdf452ae4dd3415d
Hegar Garcia <hegargarcia@gmail.com>
2020-01-22 15:55:25 -0600

    Adding handler for authorizing client Authorize clients to the Unifi Controller

---
+++ routes/auth.js
@@ -0,0 +1,43 @@
+const { Router } = require("express");
+
+const authRouter = Router();
+const fetch = require("node-fetch");
+
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+
+authRouter.post("/", async (req, res) => {
+  const uri = process.env.URI;
+
+  try {
+    const loginResponse = await fetch(`${uri}/api/login`, {
+      method: "POST",
+      body: JSON.stringify({
+        username: process.env.USERNAME,
+        password: process.env.PASSWORD
+      })
+    });
+
+    const headers = { cookie: loginResponse.headers.raw()["set-cookie"] };
+
+    await fetch(`${uri}/api/s/${process.env.SITENAME}/cmd/stamgr`, {
+      method: "POST",
+      headers,
+      body: JSON.stringify({
+        cmd: "authorize-guest",
+        mac: req.ip
+      })
+    });
+
+    await fetch(`${uri}/api/logout`, {
+      method: "POST",
+      headers
+    });
+  } catch (e) {
+    console.log(e.error);
+    console.log(e.message);
+  }
+
+  res.redirect("http://delamadrid.com.mx");
+});
+
+module.exports = authRouter;
