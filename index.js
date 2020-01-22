7e2fe96088d39c32c6e63f1af7db84e3f848cdd5
Hegar Garcia <hegargarcia@gmail.com>
2020-01-22 15:55:55 -0600

    Adding main express app

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(cors());
+
+app.use(express.static("public"));
+
+app.use(`/guest/s/${process.env.SITENAME}/`, require("./routes/public"));
+app.use("/auth", require("./routes/auth"));
+
+app.listen(3000, () => console.log("Listening"));
