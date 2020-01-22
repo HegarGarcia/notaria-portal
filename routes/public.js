da125b418f875e9c22ae347aad92e91dbad533f5
Hegar Garcia <hegargarcia@gmail.com>
2020-01-22 16:35:25 -0600

    Fix

--- routes/public.js
+++ routes/public.js
@@ -1,6 +1,9 @@
 const { Router } = require("express");
+const path = require("path");
 const publicRouter = Router();
 
-publicRouter.get("/", (_, res) => res.sendFile("index.html"));
+publicRouter.get("/", (_, res) =>
+  res.sendFile("index.html", { root: path.join(__dirname, "../public") })
+);
 
 module.exports = publicRouter;
