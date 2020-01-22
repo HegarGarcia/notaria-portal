f1f75a32a6400e3014351ece62c2b5807ecdabef
Hegar Garcia <hegargarcia@gmail.com>
2020-01-22 16:28:59 -0600

    Fixing redering index

--- routes/public.js
+++ routes/public.js
@@ -1,6 +1,9 @@
 const { Router } = require("express");
+const path = require("path");
 const publicRouter = Router();
 
-publicRouter.get("/", (_, res) => res.render("index.html"));
+publicRouter.get("/", (_, res) =>
+  res.sendFile(path.join("..", "public", "index.html"))
+);
 
 module.exports = publicRouter;
