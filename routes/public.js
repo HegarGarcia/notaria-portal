da9caa5eaf95081ee44ef6943d539d88f429aeb3
Hegar Garcia <hegargarcia@gmail.com>
2020-01-23 12:02:57 -0600

    Fixing implementation of sessions

--- routes/public.js
+++ routes/public.js
@@ -3,9 +3,7 @@ const path = require("path");
 const publicRouter = Router();
 
 publicRouter.get("/", (req, res) => {
-  req.session = {
-    macAddr: req.query.id
-  };
+  req.session.macAddr = req.query.id;
 
   res.sendFile("index.html", { root: path.join(__dirname, "../public") });
 });
